b052682d0ca18a0c14a40442ef969f39836fead0
Praveen <pkchmits@gmail.com>
2021-01-23 08:33:59 +0530

    changed module name

---
+++ loadApp/app/src/main/java/com/udacity/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.udacity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.udacity.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_detail)
+        setSupportActionBar(binding.toolbar)
+    }
+
+}
