a1f08e42d5348db7728f8ba4f4a3401379a2dfa9
Praveen <pkchmits@gmail.com>
2021-01-25 17:42:58 +0530

    Added Pending intent for opening detail activity

--- loadApp/app/src/main/java/com/udacity/NotificationUtils.kt
+++ loadApp/app/src/main/java/com/udacity/NotificationUtils.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 
 private val NOTIFICATION_ID = 0
@@ -17,6 +19,15 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // Create the content intent for the notification, which launches
     // this activity
 
+    val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+
     val notification = NotificationCompat.Builder(
         applicationContext,
         applicationContext.getString(R.string.notification_channel_id)
@@ -24,6 +35,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
         .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
         .build()
     notify(NOTIFICATION_ID, notification)
 }
